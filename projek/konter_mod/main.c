750d4cb2c3b31551e78cc0459b7ae6612fa063a3
afrendybayu <afrendy@daunbiru.com>
2013-01-21 16:45:40 +0700

    konter mod masuk

---
+++ projek/konter_mod/main.c
@@ -0,0 +1,142 @@
+/*
+	FreeRTOS.org V5.1.1 - Copyright (C) 2003-2008 Richard Barry.
+	
+	entri dari boot.s adalah ke main
+	
+	20 feb 2009
+	buat buffer file index.html dilakukan di task led.
+	
+	15 april 2009
+	coba porting untuk Komon_A (RTD & 4-20 mA)
+	
+	21 April 2009
+	Clean up untuk release stable versi 1.4
+	
+	3 Nov 2009
+	Untuk Komon_420 saja !
+*/
+
+/* Scheduler includes. */
+#include "FreeRTOS.h"
+#include "task.h"
+#include "queue.h"
+#include "semphr.h"
+#include "hardware.h"
+#include "monita/monita_uip.h"
+//#include "sys.h"
+
+unsigned int loop_idle=0;
+unsigned int idle_lama;
+unsigned int tot_idle;
+
+
+//static void sysInit(void);
+void init_led_utama(void);
+//void init_spi_mmc(char min)	;
+
+xTaskHandle hdl_led;
+xTaskHandle hdl_shell;
+xTaskHandle hdl_relay;
+
+#ifdef PAKAI_ETH
+	xTaskHandle hdl_ether;
+#endif
+
+#ifdef PAKAI_PUSHBUTTON
+	#include "monita/monita_uip.h"
+	extern unsigned int debound[KANALNYA];
+#endif
+
+
+/*-----------------------------------------------------------*/
+#define jalankan
+
+int main( void )	{
+	setup_hardware();				// @"../../hardware/hardware.h"
+
+	init_led_utama();
+	init_hardware();
+
+	vTaskStartScheduler();
+
+	printf("Keluar dari Scheduler !!!!\r\n");
+	vTaskDelay(4000);
+    /* Will only get here if there was insufficient memory to create the idle task. */
+	return 0;
+
+}
+
+
+void togle_led_utama(char tog)	{
+	//printf("debound 8 : %d, relay 8 : %s\r\n", debound[7], (data_f[(JML_SUMBER*PER_SUMBER)+7])?"Aktif":"Mati");
+	FIO0PIN ^= LED_UTAMA;
+	//printf("tot idle: %d, loop_idle: %d\r\n", tot_idle, loop_idle);
+
+	if (tog)	{
+		/* kalkulasi idle loop */
+		tot_idle = loop_idle - idle_lama;
+		idle_lama = loop_idle;
+		
+		//printf("isi: %x: %d\r\n", &MEM_RTC0+1, *(&MEM_RTC0+1));
+
+		tendang_wdog();		// reset wdog setiap detik //
+	}
+}
+
+#ifdef PAKAI_PUSHBUTTON
+	void kurangi_delay_push()	{
+		char g;
+		for (g=0; g<KANALNYA; g++) {
+			if (debound[g]) {
+				debound[g]--;
+			}
+		}
+	}
+#endif
+
+extern xTaskHandle hdl_kirimcepat;
+extern xTaskHandle hdl_ambilcepat;
+
+static portTASK_FUNCTION(task_led2, pvParameters )	{
+	static char tog = 0;
+	loop_idle = 0;
+	idle_lama = 0;
+	for (;;)	{
+		
+		#ifdef PAKAI_MODE_POWER
+		if (status_power()>0)	{
+			vTaskSuspend( hdl_kirimcepat );
+			vTaskSuspend( hdl_ambilcepat );
+			vTaskSuspend( hdl_shell );
+
+			setup_hardware();
+			xSerialPortInitMinimal(BAUD_RATE_SHELL, configMINIMAL_STACK_SIZE);
+			
+			vTaskDelay(200);
+			
+			vTaskResume( hdl_kirimcepat );
+			vTaskResume( hdl_ambilcepat );
+			//vTaskResume( hdl_shell );
+			
+			
+			flagRTCc = 0;
+			set_power_rtc(0);
+			printf("  kembali normal.... !!\r\n");
+		}
+		#endif
+		
+		togle_led_utama(tog=1-tog);
+		cek_tik();
+		
+		#ifdef PAKAI_PUSHBUTTON
+			kurangi_delay_push();
+		#endif
+		
+		vTaskDelay(500);
+		//printf(" - Tanggal   : %d-%d-%d\r\n", infoGPS.utc.day, infoGPS.utc.mon, infoGPS.utc.year);
+	}
+}
+void init_led_utama(void) {
+	xTaskCreate(task_led2, ( signed portCHAR * ) "Led2",  (configMINIMAL_STACK_SIZE * 2) ,\
+		 NULL, tskIDLE_PRIORITY - 2, ( xTaskHandle * ) &hdl_led );
+}
---
+++ projek/konter_mod/modbus.c
@@ -0,0 +1,103 @@
+
+#include "FreeRTOS.h"
+#include "task.h"
+#include "queue.h"
+#include "semphr.h"
+
+#include "mb.h"
+#include "mbport.h"
+
+//*
+
+
+#define TXDE	BIT(24)
+#define RXDE	BIT(23)
+
+#define REG_INPUT_START 1000
+#define REG_INPUT_NREGS 4
+#define REG_HOLDING_START 2000
+#define REG_HOLDING_NREGS 130
+
+/* ----------------------- Static variables ---------------------------------*/
+static USHORT   usRegInputStart = REG_INPUT_START;
+static USHORT   usRegInputBuf[REG_INPUT_NREGS];
+static USHORT   usRegHoldingStart = REG_HOLDING_START;
+static USHORT   usRegHoldingBuf[REG_HOLDING_NREGS];
+
+
+xTaskHandle hdl_modbus;
+
+static portTASK_FUNCTION(task_modbus, pvParameters )
+{
+	//init_serial3();
+	vTaskDelay(5000);
+	vTaskDelay(3000);
+	
+	#ifdef DEBUG_MODBUS_SLAVE
+	printf("init %s\r\n", __FUNCTION__);
+	#endif
+	
+	eMBInit( MB_RTU, 0x01, 19200, 1, MB_PAR_NONE );
+    
+    //eMBEnable();
+    vTaskDelay(500);
+	
+	for (;;)	{
+	//	eMBPoll();
+		vTaskDelay(1);
+
+	}
+}
+void init_task_modbus(void) {
+	xTaskCreate(task_modbus, ( signed portCHAR * ) "modbus",  (configMINIMAL_STACK_SIZE * 15) ,\
+		 NULL, tskIDLE_PRIORITY + 2, ( xTaskHandle * ) &hdl_modbus );
+}
+
+eMBErrorCode
+eMBRegInputCB( UCHAR * pucRegBuffer, USHORT usAddress, USHORT usNRegs )
+{
+    eMBErrorCode    eStatus = MB_ENOERR;
+    int             iRegIndex;
+
+    if( ( usAddress >= REG_INPUT_START )
+        && ( usAddress + usNRegs <= REG_INPUT_START + REG_INPUT_NREGS ) )
+    {
+        iRegIndex = ( int )( usAddress - usRegInputStart );
+        while( usNRegs > 0 )
+        {
+            *pucRegBuffer++ = ( unsigned char )( usRegInputBuf[iRegIndex] >> 8 );
+            *pucRegBuffer++ = ( unsigned char )( usRegInputBuf[iRegIndex] & 0xFF );
+            iRegIndex++;
+            usNRegs--;
+        }
+    }
+    else
+    {
+        eStatus = MB_ENOREG;
+    }
+
+    return eStatus;
+}
+
+eMBErrorCode eMBRegHoldingCB( UCHAR * pucRegBuffer, USHORT usAddress, USHORT usNRegs, eMBRegisterMode eMode ) {
+    ( void )pucRegBuffer;
+    ( void )usAddress;
+    ( void )usNRegs;
+    ( void )eMode;
+    return MB_ENOREG;
+}
+
+eMBErrorCode eMBRegCoilsCB( UCHAR * pucRegBuffer, USHORT usAddress, USHORT usNCoils, eMBRegisterMode eMode )	{
+    ( void )pucRegBuffer;
+    ( void )usAddress;
+    ( void )usNCoils;
+    ( void )eMode;
+    return MB_ENOREG;
+}
+
+eMBErrorCode eMBRegDiscreteCB( UCHAR * pucRegBuffer, USHORT usAddress, USHORT usNDiscrete )	{
+    ( void )pucRegBuffer;
+    ( void )usAddress;
+    ( void )usNDiscrete;
+    return MB_ENOREG;
+}
